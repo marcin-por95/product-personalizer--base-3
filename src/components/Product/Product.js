750cc1499fbbff2dab06c5c752ef4da7d2766603
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-06-05 16:24:57 +0200

    adjustment product component to get colors

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,22 @@
 import styles from './Product.module.scss';
-import clsx from 'clsx';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
 
 const Product = props => {
-  const { title, basePrice } = props;
+  const { title, basePrice, colors, sizes, name  } = props; // Destructure the props
+
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [selectedSize, setSelectedSize] = useState(sizes[0]);
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${selectedColor}.jpg`} />
       </div>
       <div>
         <header>
@@ -21,18 +27,35 @@ const Product = props => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map((size) => (
+                  <li key={size.name}>
+                    <button
+                        type="button"
+                        className={clsx(selectedSize === size.name && styles.active)}
+                        onClick={() => setSelectedSize(size.name)}
+                    >
+                      {size.name}
+                    </button>
+                  </li>
+              ))}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map((color) => (
+                  <li key={color}>
+                    <button
+                        type="button"
+                        className={clsx(
+                            styles.colorButton,
+                            selectedColor === color && styles.active
+                        )}
+                        style={{ backgroundColor: color }}
+                        onClick={() => setSelectedColor(color)}
+                    />
+                  </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -44,4 +67,15 @@ const Product = props => {
   )
 };
 
+Product.propTypes = {
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  sizes: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        additionalPrice: PropTypes.number.isRequired,
+      })
+  ).isRequired,
+};
 export default Product;
